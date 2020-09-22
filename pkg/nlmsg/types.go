d765cddb4085b4c61a58f48c7cffd1ef650e8401
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2020-09-22 19:23:26 +0900

    2020.09.22-19:23

--- pkg/nlmsg/types.go
+++ pkg/nlmsg/types.go
@@ -6,6 +6,15 @@ import (
 )
 
 type NlmsgType uint16
+type NlmsgFlags uint16
+
+type Header struct {
+	Len   uint32
+	Type  NlmsgType
+	Flags NlmsgFlags
+	Seq   uint32
+	Pid   uint32
+}
 
 func (nt NlmsgType) String() string {
 	switch nt {
@@ -93,10 +102,52 @@ func (nt NlmsgType) String() string {
 	return fmt.Sprintf("UNKNOWN(%d)", nt)
 }
 
-type Header struct {
-	Len   uint32
-	Type  NlmsgType
-	Flags uint16
-	Seq   uint32
-	Pid   uint32
+func (f NlmsgFlags) String() string {
+	str := ""
+	if f&syscall.NLM_F_REQUEST != 0 {
+		str += "REQUEST|"
+	}
+	if f&syscall.NLM_F_ACK != 0 {
+		str += "ACK|"
+	}
+	if f&syscall.NLM_F_APPEND != 0 {
+		str += "APPEND|"
+	}
+	if f&syscall.NLM_F_ATOMIC != 0 {
+		str += "ATOMIC|"
+	}
+	if f&syscall.NLM_F_CREATE != 0 {
+		str += "CREATE|"
+	}
+	if f&syscall.NLM_F_DUMP != 0 {
+		str += "DUMP|"
+	}
+	if f&syscall.NLM_F_ECHO != 0 {
+		str += "ECHO|"
+	}
+	if f&syscall.NLM_F_EXCL != 0 {
+		str += "EXCL|"
+	}
+	if f&syscall.NLM_F_MATCH != 0 {
+		str += "MATCH|"
+	}
+	if f&syscall.NLM_F_MULTI != 0 {
+		str += "MULTI|"
+	}
+	if f&syscall.NLM_F_REPLACE != 0 {
+		str += "REPLACE|"
+	}
+	if f&syscall.NLM_F_REQUEST != 0 {
+		str += "REQUEST|"
+	}
+	if f&syscall.NLM_F_ROOT != 0 {
+		str += "ROOT|"
+	}
+
+	if str == "" {
+		str = "<n/a>"
+	} else {
+		str = fmt.Sprintf("<%s>", str[:len(str)-2])
+	}
+	return str
 }
