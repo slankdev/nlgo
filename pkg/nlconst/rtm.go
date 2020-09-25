42752e77b0c5d19bbd7ad5da6e63a0a4a748612b
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2020-09-25 14:00:25 +0900

    2020.09.25-14:00

--- pkg/nlconst/rtm.go
+++ pkg/nlconst/rtm.go
@@ -2,9 +2,11 @@ package nlconst
 
 import (
 	"fmt"
+	"syscall"
 )
 
 type NlmsgType uint16
+type NlmsgFlags uint16
 
 const (
 	RTM_BASE = 16
@@ -233,4 +235,54 @@ func (t NlmsgType) String() string {
 	default:
 		return fmt.Sprintf("UNKNOWN(%d)", t)
 	}
+}
+
+func (f NlmsgFlags) String() string {
+	str := ""
+	if f&syscall.NLM_F_REQUEST != 0 {
+		str += "REQUEST|"
+	}
+	if f&syscall.NLM_F_ACK != 0 {
+		str += "ACK|"
+	}
+	if f&syscall.NLM_F_APPEND != 0 {
+		str += "APPEND|"
+	}
+	if f&syscall.NLM_F_ATOMIC != 0 {
+		str += "ATOMIC|"
+	}
+	if f&syscall.NLM_F_CREATE != 0 {
+		str += "CREATE|"
+	}
+	if f&syscall.NLM_F_DUMP != 0 {
+		str += "DUMP|"
+	}
+	if f&syscall.NLM_F_ECHO != 0 {
+		str += "ECHO|"
+	}
+	if f&syscall.NLM_F_EXCL != 0 {
+		str += "EXCL|"
+	}
+	if f&syscall.NLM_F_MATCH != 0 {
+		str += "MATCH|"
+	}
+	if f&syscall.NLM_F_MULTI != 0 {
+		str += "MULTI|"
+	}
+	if f&syscall.NLM_F_REPLACE != 0 {
+		str += "REPLACE|"
+	}
+	if f&syscall.NLM_F_REQUEST != 0 {
+		str += "REQUEST|"
+	}
+	if f&syscall.NLM_F_ROOT != 0 {
+		str += "ROOT|"
+	}
+
+	if str == "" {
+		str = fmt.Sprintf("<(%d)>", f)
+	} else {
+		str = fmt.Sprintf("<(%d)%s>", f, str[:len(str)-2])
+	}
+	return str
 }
--- pkg/nlmsg/types.go
+++ pkg/nlmsg/types.go
@@ -1,68 +1,13 @@
 package nlmsg
 
 import (
-	"fmt"
-	"syscall"
-
 	"github.com/slankdev/nlgo/pkg/nlconst"
 )
-
-type NlmsgFlags uint16
 
 type Header struct {
 	Len   uint32
 	Type  nlconst.NlmsgType
-	Flags NlmsgFlags
+	Flags nlconst.NlmsgFlags
 	Seq   uint32
 	Pid   uint32
-}
-
-func (f NlmsgFlags) String() string {
-	str := ""
-	if f&syscall.NLM_F_REQUEST != 0 {
-		str += "REQUEST|"
-	}
-	if f&syscall.NLM_F_ACK != 0 {
-		str += "ACK|"
-	}
-	if f&syscall.NLM_F_APPEND != 0 {
-		str += "APPEND|"
-	}
-	if f&syscall.NLM_F_ATOMIC != 0 {
-		str += "ATOMIC|"
-	}
-	if f&syscall.NLM_F_CREATE != 0 {
-		str += "CREATE|"
-	}
-	if f&syscall.NLM_F_DUMP != 0 {
-		str += "DUMP|"
-	}
-	if f&syscall.NLM_F_ECHO != 0 {
-		str += "ECHO|"
-	}
-	if f&syscall.NLM_F_EXCL != 0 {
-		str += "EXCL|"
-	}
-	if f&syscall.NLM_F_MATCH != 0 {
-		str += "MATCH|"
-	}
-	if f&syscall.NLM_F_MULTI != 0 {
-		str += "MULTI|"
-	}
-	if f&syscall.NLM_F_REPLACE != 0 {
-		str += "REPLACE|"
-	}
-	if f&syscall.NLM_F_REQUEST != 0 {
-		str += "REQUEST|"
-	}
-	if f&syscall.NLM_F_ROOT != 0 {
-		str += "ROOT|"
-	}
-
-	if str == "" {
-		str = fmt.Sprintf("<(%d)>", f)
-	} else {
-		str = fmt.Sprintf("<(%d)%s>", f, str[:len(str)-2])
-	}
-	return str
 }
